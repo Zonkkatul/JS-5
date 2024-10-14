3f366e0f0f334e9080def21055d088de3ae23973
Zonkkatul <mervezonkk@gmail.com>
2024-10-14 15:26:06 +0200

    js exercises final

---
+++ ex3.js
@@ -0,0 +1,12 @@
+let number1 = 11;
+let number2 = 5;
+
+
+
+console.log("Addition: " + (number1 + number2));    
+
+console.log("Subtraction: " + (number1 - number2));   
+
+console.log("Multiplication: " + (number1 * number2)); 
+
+console.log("Division: " + (number1 / number2)); 
