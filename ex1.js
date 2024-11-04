f3818aa1bd61b98f24def6d15d150b05ee088c85
Zonkkatul <mervezonkk@gmail.com>
2024-11-04 20:40:29 +0100

    ex3

--- ex1.js
+++ ex1.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// EXERCISE 3
+
+function sumEvenNumbers(array) {
+    let sum = 0;
+    for(let i = 0; i<array.length;i++) {
+        if(array[i] % 2 == 0) {
+            sum += array[i];
+        }
+    }
+
+    return sum;
+}
--- ex2.js
+++
@@ -1,5 +0,0 @@
-let myName = "Merve";
-let myAge = 38;
-let myfavoriteColor = "blue";
-
-console.log("My name is " +myName + ", i am " + myAge + " years old. My favorite color is " + myfavoriteColor + "." );
