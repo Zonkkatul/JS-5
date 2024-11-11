cc863126b33dafe31fa2a0d6e2dac44ad08f6826
Zonkkatul <mervezonkk@gmail.com>
2024-11-11 09:57:50 +0100

    bank account first commit

--- ex1.js
+++ ex1.js
@@ -1,12 +1,39 @@
-// EXERCISE 3
-
-function sumEvenNumbers(array) {
-    let sum = 0;
-    for(let i = 0; i<array.length;i++) {
-        if(array[i] % 2 == 0) {
-            sum += array[i];
-        }
+const account = {
+    accountName: "John Doe", 
+    balance: 1000,           
+  
+    
+    getBalance: function () {
+      console.log(`Total balance: $${this.balance}`);
+    },
+  
+  
+    deposit: function (amount) {
+      if (amount > 0) {
+        this.balance += amount;
+        console.log(`Deposited $${amount}. New balance: $${this.balance}`);
+      } else {
+        this.accountError("Invalid deposit amount.");
+      }
+    },
+  
+    
+    withdrawal: function (amount) {
+      if (amount > 0 && amount <= this.balance) {
+        this.balance -= amount;
+        console.log(`Withdrew $${amount}. New balance: $${this.balance}`);
+      } else {
+        this.accountError("Invalid withdrawal amount or insufficient funds.");
+      }
+    },
+  
+    
+    getAccountName: function () {
+      console.log(`Account Holder: ${this.accountName}`);
+    },
+  
+    
+    accountError: function (message) {
+      console.log(`Error: ${message}`);
     }
-
-    return sum;
-}
+  };
