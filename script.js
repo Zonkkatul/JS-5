7654196b29420be4dc44382028b5995a8a1d34f1
Zonkkatul <mervezonkk@gmail.com>
2024-11-19 12:44:45 +0100

    part2

--- script.js
+++ script.js
@@ -22,3 +22,72 @@ const words = [
   "loving",
   "north",
 ];
+
+const input = document.getElementById("text");
+
+const scoreElement = document.getElementById("score");
+const timeElement = document.getElementById("time");
+const timeInterval = setInterval (updateTime, 1000);
+
+let correctWord = addWordToDom();
+
+let score = 0;
+let time = 30;
+
+
+function addWordToDom() {
+  const randomIndex = Math.floor(Math.random() * (words.length));
+
+  
+  const word = words[randomIndex];
+
+  document.getElementById("word").innerHTML = word;
+  return word;
+}
+
+function updateTime() {
+  time --;
+  timeElement.innerHTML =  `Time: ${time}`;
+
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameover();
+  }
+}
+
+
+input.addEventListener("keypress", function(event){
+  if (event.key === "Enter") {
+if (this.value === correctWord) {
+  score++;
+  scoreElement.innerHTML= `Score: ${score}`;
+
+  correctWord = addWordToDom();
+
+  time += 5;
+  timeElement.innerHTML = `Time: ${time}`;
+
+ } else {
+    console.log("try again!");
+  
+}
+
+
+this.value = "";
+  }
+})
+
+function gameover() {
+  document.querySelector("h2").innerText = `Game Over!`;
+}
+
+
+
+
+
+
+
+
+
+
