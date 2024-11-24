dc44363e686177f4b823cddf39102f417a48c351
Zonkkatul <mervezonkk@gmail.com>
2024-11-24 12:56:05 +0100

    adding extra events

--- script.js
+++ script.js
@@ -28,13 +28,17 @@ const input = document.getElementById("text");
 const scoreElement = document.getElementById("score");
 const timeElement = document.getElementById("time");
 const timeInterval = setInterval (updateTime, 1000);
+const settingsBtn = document.getElementById("settings-btn");
+const difficultySelect = document.getElementById("difficulty");
+
+let difficulty = 'medium';
 
 let correctWord = addWordToDom();
 
 let score = 0;
 let time = 30;
 
-
+//Adding word to DOM
 function addWordToDom() {
   const randomIndex = Math.floor(Math.random() * (words.length));
 
@@ -45,6 +49,8 @@ function addWordToDom() {
   return word;
 }
 
+
+//Updating Time
 function updateTime() {
   time --;
   timeElement.innerHTML =  `Time: ${time}`;
@@ -56,7 +62,7 @@ function updateTime() {
   }
 }
 
-
+//Input
 input.addEventListener("keypress", function(event){
   if (event.key === "Enter") {
 if (this.value === correctWord) {
@@ -65,22 +71,46 @@ if (this.value === correctWord) {
 
   correctWord = addWordToDom();
 
-  time += 5;
-  timeElement.innerHTML = `Time: ${time}`;
 
- } else {
-    console.log("try again!");
-  
-}
-
-
-this.value = "";
+  //Increase or decrease time based on difficulty
+  if (difficulty === 'easy') {
+    time += 5;
+  } else if (difficulty === 'medium') {
+    time += 3;
+  } else if (difficulty === 'hard') {
+    time += 2;
   }
-})
+  timeElement.innerHTML = `Time: ${time}`;
+} else {
+  console.log("try again!");
+}
+this.value = "";
+}
+});
 
+
+//Game Over 
 function gameover() {
   document.querySelector("h2").innerText = `Game Over!`;
+
 }
+
+
+//Toggle- Setting Visibility
+settingsBtn.addEventListener("click", () => {
+  const settings = document.getElementById("settings");
+  settings.classList.toggle("hidden");
+});
+
+
+//Changing Difficulty
+settingsForm.addEventListener("change", (e) => {
+  if (e.target.id === "difficulty") {
+    difficulty = e.target.value;
+  }
+});
+
+
 
 
 
